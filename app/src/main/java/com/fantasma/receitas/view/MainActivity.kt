2860636d05cad06c963dab4c86d104c3bbac8113
Ramon de Guimarães Apolinario <r4mon.guimaraes@gmail.com>
2021-11-29 19:37:32 -0300

    #15 feat: Adicionando base para navegacao entre telas

--- app/src/main/java/com/fantasma/receitas/view/MainActivity.kt
+++ app/src/main/java/com/fantasma/receitas/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fantasma.receitas
+package com.fantasma.receitas.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fantasma.receitas.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
